36dcb5e5b4a50e06aad7cf28d12665fe44d7bab3
elara\nudzejma.hanic <nudzejma.hanic@sme.additiv.com>
2024-09-18 00:07:28 +0200

    feat: setup fixes regardin tailwind.config and globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		'./app/**/*.{ts,tsx}',
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/features/**/*.{js,ts,jsx,tsx,mdx}",
 	],
