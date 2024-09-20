918f28ec4cce800c8d2080c3361f74061595912b
elara\nudzejma.hanic <nudzejma.hanic@sme.additiv.com>
2024-09-20 22:33:57 +0200

    feat: imported RoomList in page

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,7 +1,23 @@
-import Auth from '@/features/auth/components/auth';
 
-export default function Home() {
+import React from "react";
+import RoomList from "@/components/room/room-list";
+import { getRooms } from "@/actions/get-rooms";
+
+export type HomeProps = {
+  search: {
+    name: string,
+    capacity: number
+  }
+}
+
+export default async function Home({ search }: HomeProps) {
+  const rooms = await getRooms(search?.name);
+
+  if (!rooms) return <>No rooms found!</>
+
   return (
-    <Auth />
-  );
+    <>
+      <RoomList rooms={rooms} />
+    </>
+  )
 }
