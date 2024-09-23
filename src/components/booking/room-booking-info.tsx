4fff391923ecccabb92c2ddd090b1364201d3900
elara\nudzejma.hanic <nudzejma.hanic@sme.additiv.com>
2024-09-23 04:15:37 +0200

    feat: updated room-booking info with back button

--- src/components/booking/room-booking-info.tsx
+++ src/components/booking/room-booking-info.tsx
@@ -3,10 +3,8 @@
 import React from 'react';
 import useBookRoom from '@/app/store/use-book-room.store';
 import { useRouter } from 'next/navigation';
-
 import Title, { TypeEnum } from '@/components/ui/title';
 import { Button } from '@/components/ui/button';
-
 import moment from 'moment';
 
 const RoomBookingInfo = () => {
@@ -22,14 +20,20 @@ const RoomBookingInfo = () => {
 
     return (
         <div className="flex flex-col p-6 max-w-md mx-auto">
-            <Title as="h5" type={TypeEnum.cardMain} className="text-center mb-4">Your Booking Summary</Title>
+            <Title as="h5" type={TypeEnum.cardMain} className="text-center text-xl font-semibold mb-4">Your Booking Summary</Title>
             <div className="mb-6 text-center text-gray-700">
-                <div className="text-lg">Check-in Date: <span className="font-semibold text-gray-800">{date}</span></div>
+                <div className="text-lg mb-1">Check-in Date: <span className="font-semibold text-gray-800">{date}</span></div>
                 <div className="text-lg">Booking Hours: <span className="font-semibold text-gray-800">{bookingRoomData?.timeSlot}</span></div>
             </div>
-            <Button onClick={handleBookingInfo} className='max-width-[150px]'>
-                Check Your Bookings
-            </Button>
+
+            <div className='flex justify-center gap-4'>
+                <Button onClick={() => router.push('/')} variant='outline' className="bg-transparent hover:bg-gray-100">
+                    Go Home
+                </Button>
+                <Button onClick={handleBookingInfo}>
+                    Check Your Bookings
+                </Button>
+            </div>
         </div>
     );
 }
